e9b478e04c78ebc66c025b583bcbe1b9e7f688d1
alvaro.obregon <alvaro911>
2023-08-28 22:46:38 -0500

    configuaracion de ambiente

---
+++ WORKSPACE/TestDos/src/com/alvaro/principal/Partida.java
@@ -0,0 +1,24 @@
+package com.alvaro.principal;
+
+public class Partida {
+
+	public static void main(String[] arg) {
+		
+		System.out.print("Mensaje de bienvenida \r\n");
+		
+		System.out.print("1\t\tUNO\r\n");
+		System.out.print("10\t\tDiez\r\n");
+		System.out.print("100000000000\tCien mil\r\n");
+		
+		
+		
+		
+		System.out.println("E:\\Mis Docs\\Curso Java Intermedio\\WORKSPACE 1");
+		
+		System.out.println(" -Juanito- exclamo -- \"AHHHHHHHHH\" ");
+		
+		System.out.print("!!!!");
+		
+		
+	}
+}
