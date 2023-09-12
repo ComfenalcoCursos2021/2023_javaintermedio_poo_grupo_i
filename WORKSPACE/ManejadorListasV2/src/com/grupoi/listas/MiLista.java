2a558a206a5a833aca5ef86488b95f8c82e715cf
alvaro.obregon <alvaro911>
2023-09-11 21:33:47 -0500

    20230911 v2

--- WORKSPACE/ManejadorListasV2/src/com/grupoi/listas/MiLista.java
+++ WORKSPACE/ManejadorListasV2/src/com/grupoi/listas/MiLista.java
@@ -1,35 +1,45 @@
 package com.grupoi.listas;
 
-
 public class MiLista {
-	
+
 	private CajaNegra primerElemento;
-	
-	
+
 	public void agregar(int pepito) {
-		if(primerElemento == null) {
+		if (primerElemento == null) {
 			CajaNegra nueva = new CajaNegra();
 			nueva.setDato(pepito);
 			primerElemento = nueva;
 		} else {
-			
+
 			CajaNegra temporal = primerElemento;
-			while(temporal.getSiguiente() != null) {
+			while (temporal.getSiguiente() != null) {
 				temporal = temporal.getSiguiente();
-			}		
+			}
 			CajaNegra nueva = new CajaNegra();
 			nueva.setDato(pepito);
 			temporal.setSiguiente(nueva);
 			System.out.println("---------------");
 		}
 	}
+
 	public void imprimirPantalla() {
 		CajaNegra temporal = primerElemento;
-		while(temporal.getSiguiente() != null) {
+		while (temporal.getSiguiente() != null) {
 			System.out.println(temporal.getDato());
 			temporal = temporal.getSiguiente();
-		}	
+		}
 		System.out.println(temporal.getDato());
 	}
-	
+
+	public void borrarUltimo() {
+		CajaNegra temporal = primerElemento; // 1 2 3 4 5
+		while (temporal.getSiguiente().getSiguiente() != null) {
+			temporal = temporal.getSiguiente();
+		}
+
+		temporal.setSiguiente(null);
+		System.out.println("Tú último dato no está");
+
+	}
+
 }
