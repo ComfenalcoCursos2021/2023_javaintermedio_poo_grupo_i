be7a8c7dc9086c33e5c31e05479aa5cd3d5d07bf
alvaro.obregon <alvaro911>
2023-09-19 18:54:17 -0500

    Clase 20230919

--- WORKSPACE/ElseIf/src/com/grupoi/main/Main.java
+++ WORKSPACE/ElseIf/src/com/grupoi/main/Main.java
@@ -28,14 +28,32 @@ public class Main {
 
 		if (numero == 1) {
 			System.out.println("Uno");
-		} else if (numero == 2) {
+		} else if (numero == 2 && numero < 5 ) {
 			System.out.println("Dos");
 		} else if (numero == 3) {
 			System.out.println("Tres");
 		} else if (numero == 4) {
 			System.out.println("Cuatro");
 		} else if (numero == 5) {
-			System.out.println("Cinco");
+			System.out.println("Cinco");			
+		}
+		
+		switch (numero) {
+		case 1:
+			System.out.println("Uno");
+			break;
+		case 2:
+			System.out.println("Dos");
+			break;
+		case 3:
+			System.out.println("Tres");
+			break;
+		case 4:
+			System.out.println("Cuatro");
+			break;
+		case 5:
+			System.out.println("Cinco");			
+			break;		
 		}
 	}
 
