68822de1d7a1cabad4f85bfb04cf4282ac01a536
alvaro.obregon <alvaro911>
2023-09-21 20:46:49 -0500

    Clase 20230921 v1

---
+++ WORKSPACE/SandBoxValdesV2/src/com/grupoi/principal/Main.java
@@ -0,0 +1,13 @@
+package com.grupoi.principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		System.out.println("Termino!!!");
+
+	}
+
+}
